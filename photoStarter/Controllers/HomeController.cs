1418d735e7ca7b55abbc4a7c60ce6df7d772b43c
Ben Holden <holden.ben@gmail.com>
2013-06-19 19:00:33 +0100

    initial commit

---
+++ photoStarter/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace photoStarter.Controllers {
+	public class HomeController : Controller {
+		public ActionResult Index() {
+			ViewBag.Title = "Home";
+			ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+			return View();
+		}
+	}
+}
---
+++ photoStarter/ServiceLayer/FlickrBLL.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Linq;
+using FlickrNet;
+
+namespace photoStarter.ServiceLayer
+{
+	/// <summary>
+	/// Helper class for confortable pagining and binding
+	/// </summary>
+	[DataObject(true)]
+	[Serializable]
+	public class FlickrBLL {
+		[DataObjectMethodAttribute(DataObjectMethodType.Select, true)]
+		public static PhotosetPhotoCollection GetPagedSet(string setId, int maximumRows, int startRowIndex) {
+			Flickr flickr = new Flickr(ConfigurationManager.AppSettings["apiKey"],
+				ConfigurationManager.AppSettings["sharedSecret"]);
+			PhotosetPhotoCollection photos = flickr.PhotosetsGetPhotos(setId, GetPageIndex(
+				startRowIndex, maximumRows) + 1, maximumRows);
+
+			return photos;
+		}
+
+		public static int GetPagedSetCount(string setId) {
+			Flickr flickr = new Flickr(ConfigurationManager.AppSettings["apiKey"],
+				ConfigurationManager.AppSettings["sharedSecret"]);
+			Photoset set = flickr.PhotosetsGetInfo(setId);
+			return set.NumberOfPhotos;
+		}
+
+		[DataObjectMethodAttribute(DataObjectMethodType.Select, false)]
+		public static PhotosetCollection GetPhotoSetsByUser(string userId) {
+			Flickr flickr = new Flickr(ConfigurationManager.AppSettings["apiKey"],
+				ConfigurationManager.AppSettings["sharedSecret"]);
+
+			return flickr.PhotosetsGetList(userId);
+		}
+
+		protected static int GetPageIndex(int startRowIndex, int maximumRows) {
+			if (maximumRows <= 0)
+				return 0;
+			else
+				return (int)Math.Floor((double)startRowIndex / (double)maximumRows);
+		}
+
+		//public static 
+
+
+	}
+}
